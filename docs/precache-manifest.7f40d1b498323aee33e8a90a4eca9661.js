24344b2d547c89c2d4b6835d26df99e405495e12
Juan van den Anker <juan@vdanker.net>
2019-05-07 07:45:46 +1200

    github pages

---
+++ docs/precache-manifest.7f40d1b498323aee33e8a90a4eca9661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c547af2fe014234dfbebaf4651c41380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "331b2a8147c4dfa6e054",
+    "url": "/static/css/main.799c6449.chunk.css"
+  },
+  {
+    "revision": "21e44c16a6d21f0c6382",
+    "url": "/static/js/2.90635ffc.chunk.js"
+  },
+  {
+    "revision": "331b2a8147c4dfa6e054",
+    "url": "/static/js/main.4aedc25d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
